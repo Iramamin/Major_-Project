4c2ea5c569eefd157acb6d3cff7417f73a86134b
iram amin <iramamin428@gmail.com>
2024-03-08 01:55:58 +0530

    make many changes

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 //code for connection to local host and acquiring mongo ,express etc(Basic setup).
-const express = require ("express");
-const mongoose=require("mongoose");
+const express = require ('express');
 const app = express();
-const path=require("path");
-
-
+const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
-});
-//basic api 
-app.get("/",()=>{
-    console.log("welcome to port");
-});
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+
+
+//connect to mongodb
 main()
 .then(()=>{
 console.log("connection sucussful");
@@ -23,18 +19,83 @@ console.log("connection sucussful");
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-//
-app.get("/testListing",(req,res)=>{
- res.send("hi i am root");
+
+
+app.listen(8080,()=>{
+    console.log("server is listening on port 8080");
 });
 
-//index rout
-app.get("/listings",async(req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("/Listings/index.ejs",{allListings});
+//setup view engine and middleware
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+//Basic API
+app.get("/",()=>{
+console.log("welcome to port");
 });
+
+//initial route
+app.get("/testListing",(req,res)=>{
+res.send("hi i am root");
+});
+
+
+//index route
+app.get("/listings",async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("/listings/index.ejs",{allListings});
+});
+
+
+//new route
+app.get("./listings/new",(req,res)=>{
+ res.render("listings/new.ejs");
+});
+
+//show route 
+app.get("/listings/:id", async(req,res)=>{
+  let {id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listing/show.ejs",{listing});
+});
+
+
+//create route
+app.post("/listings",async(req,res)=>{
+const newListing = new Listing(req.body.listing);
+await newListing.save();
+res.redirect("/listings");
+});
+
+
+//edit route
+app.get("/listings/:id/edit", async(req,res)=>{
+let {id}=req.params;
+const listing=await Listing.findById(id);
+res.render("listings/edit.ejs",{listing});
+});
+
+//update route
+app.put("/listing/:id",async(req,res)=>{
+let {id}=req.params;
+await Listing.findByIdAndUpdate(id,{...req.body.listing});
+res.redirect(`/listings ${id}` );
+
+});
+
+//delete route
+app.delete("/listings/id:",async (req,res)=>{
+let {id}=req.params;
+let deletedListing=await Listing.findByIdAndDelete(id);
+console.log(deletedListing);
+res.redirect("/listings");
+});
+
+
 
 
 
